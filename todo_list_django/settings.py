c563b78cd1ba9ce1ee35e4b1c0ccd57f9b25fb9b
Diego Costa <diegocostacmp@gmail.com>
2020-03-03 00:00:19 -0300

    Bug fixing in deploy

--- todo_list_django/settings.py
+++ todo_list_django/settings.py
@@ -127,8 +127,18 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 AUTH_USER_MODEL = 'core.User'
 
